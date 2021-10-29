64a6b46260113810c609e7ed63a6fd1be210a7f1
Andrei Korikov <kpanic666@gmail.com>
2021-10-29 17:53:17 +0500

    Initial Commit

---
+++ CaptureThis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaptureThis
+//
+//  Created by Andrei Korikov on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
